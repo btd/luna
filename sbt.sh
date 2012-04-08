b98c898b8783120597ce591803878d46d87b7974
Denis Bardadym <bardadymchik@gmail.com>
2012-04-08 21:05:33 +0400

    Return missing "$@"

--- sbt.sh
+++ sbt.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx712M -Xss2M -jar "sbt-launch.jar"
+java -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx712M -Xss2M -jar "sbt-launch.jar" "$@"
