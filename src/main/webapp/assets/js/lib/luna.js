a8c066cc4d14577e093fee0fb32b88118f15c7c5
Denis Bardadym <bardadymchik@gmail.com>
2012-07-30 22:17:18 +0400

    Save access_token to localStorage

--- src/main/webapp/assets/js/lib/luna.js
+++ src/main/webapp/assets/js/lib/luna.js
@@ -1,21 +1,44 @@
 define(['jquery'], function($) {
   var Luna = function() {
     var version = 1;
+
+    var addToken = function(data, token) {
+      if(token) {
+        $.extend(data, {
+          access_token: token
+        });
+      }
+      return data;
+    };
   
     return {
       setupFail: function(jqXHR, textStatus, errorThrown) {
         console.log(jqXHR.status + ": " + jqXHR.responseText);
       },
+
+      getToken: function() {
+        return this.access_token || (localStorage && localStorage.getItem("access_token"));
+      },
+
       authorize: function(data, success, error) {
-        $.ajax({
-          dataType: 'json',
-          url: '/api/' + version + "/auth/token",
-          data: data,
-          type: 'GET'
-        }).done(function(data) {
-          this.access_token = data.access_token;
-          success(data);
-        }).fail(error || this.setupFail);
+        var self = this;
+        try {
+          $.ajax({
+            dataType: 'json',
+            url: '/api/' + version + "/auth/token",
+            data: addToken(data || {}, self.getToken()),
+            type: 'GET'
+          }).done(function(data) {
+            self.access_token = data.access_token;
+            localStorage && localStorage.setItem("access_token", self.access_token);
+            success(data);
+          }).fail(function(jqXHR, textStatus, errorThrown) {
+            localStorage && localStorage.removeItem("access_token", self.access_token);
+            (error || self.setupFail).apply(this, arguments)
+          });
+        } catch(exception) {
+          
+        }
       },
       mainWiki: function(success) {
         $.ajax({
